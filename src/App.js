7b98a616cf9c2035f84917cffa1ddbcc7d1e6837
Joz-bruer Quince <jquince@e1z3r1p16.42.us.org>
2019-12-14 20:08:19 -0800

    Error, try to fix the navbar prob

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navigationbar from './components/CustomNavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <div>
+    <Navigationbar/>
+     <Router>
+        <Route exact path="/" component={ Home } ></Route>
+        <Route  path="/About" component={ About } ></Route>
+        <Route  path="/Contact" component={ Contact } ></Route>
+     </Router>
+   </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
-import '../assets/styles/about.css';
+import '../assets/styles/contact.css';
 
 
 
-function About (){
+function Contact (){
     return(
-        <h1>ABOUT PAGE</h1>
+        <h1>CONTACT PAGE</h1>
     )
 }
 
-export default About;
+export default Contact;
